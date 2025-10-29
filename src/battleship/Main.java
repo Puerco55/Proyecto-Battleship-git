7a739db8509e7e9a548ac8ab46c71b428e112dc0
e.ureta <e.ureta@10.164.55.124>
2025-10-29 12:46:25 +0100

    Implementación de botón de estadísticas, y confirmación de cierre de ventana

--- src/battleship/Main.java
+++ src/battleship/Main.java
@@ -1,5 +1,17 @@
 package battleship;
 
+import javax.swing.SwingUtilities;
+
 public class Main {
+    public static void main(String[] args) {
+        // Es una buena práctica iniciar las aplicaciones Swing de esta manera.
+        // Asegura que la GUI se cree en el hilo de despacho de eventos (EDT).
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Menu();
+            }
+        });
+    }
 
 }
