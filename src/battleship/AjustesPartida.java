dd4915824a3dff4f143bfda1ee3105b112269cb4
FACIIAN <ian.anaya@opendeusto.es>
2025-11-24 08:39:31 +0100

    Cambio en valores iniciales de ajustes de partida

--- src/battleship/AjustesPartida.java
+++ src/battleship/AjustesPartida.java
@@ -45,17 +45,17 @@ public class Ajustes_Partida extends JFrame {
 			gbc.gridx = 1;
 			
 			if (i == 0) {
-				spinners[i] = new JSpinner(new SpinnerNumberModel(1, 0, 1, 1)); // Barcos de 5 (min = 0, max = 1)
+				spinners[i] = new JSpinner(new SpinnerNumberModel(0, 0, 1, 1)); // Barcos de 5 (min = 0, max = 1)
 			} else if (i == 1) {
-				spinners[i] = new JSpinner(new SpinnerNumberModel(1, 0, 2, 1)); // Barcos de 4 (min = 0, max = 2)
+				spinners[i] = new JSpinner(new SpinnerNumberModel(0, 0, 2, 1)); // Barcos de 4 (min = 0, max = 2)
 			} else if (i == 2) {
-				spinners[i] = new JSpinner(new SpinnerNumberModel(1, 0, 2, 1)); // Barcos de 3 (min = 0, max = 2)
+				spinners[i] = new JSpinner(new SpinnerNumberModel(0, 0, 2, 1)); // Barcos de 3 (min = 0, max = 2)
 			} else if (i == 3) {
-				spinners[i] = new JSpinner(new SpinnerNumberModel(1, 0, 5, 1)); // Barcos de 2 (min = 0, max = 5)
+				spinners[i] = new JSpinner(new SpinnerNumberModel(0, 0, 5, 1)); // Barcos de 2 (min = 0, max = 5)
 			} else if (i == 4) {
-				spinners[i] = new JSpinner(new SpinnerNumberModel(1, 0, 1, 1));
+				spinners[i] = new JSpinner(new SpinnerNumberModel(0, 0, 1, 1));
 			} else if (i == 5) {
-				spinners[i] = new JSpinner(new SpinnerNumberModel(1, 0, 1, 1));
+				spinners[i] = new JSpinner(new SpinnerNumberModel(0, 0, 1, 1));
 			}
 			
 			spinners[i].setPreferredSize(new Dimension(50, 25));
